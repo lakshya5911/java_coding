fade24c0626c9f0f80bd12497171f230da970322
lakshya <134369201+lakshya5911@users.noreply.github.com>
2023-05-25 21:52:08 +0530

    upload java array code

---
+++ array_creation.java
@@ -0,0 +1,36 @@
+package Array;
+
+public class array_creation {
+    
+    // How to create an array in one dimensional array
+
+    public static void main (String[] args){
+        // LOOP POOL -
+        // conclusion =  we can convert value int into       byte = short = int = char 
+        
+        int[] a = new int[2] ;
+        int[] b = new int[0] ;
+        // To convert into byte into int
+        byte  c = 10 ;
+        int[] d = new int[c] ;
+        char  e = 20 ;
+        int[] f = new int[e] ;
+        short g = 30 ;
+        int[] h = new int[g] ;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(h);
+
+
+
+
+
+
+
+
+    }
+    
+}
